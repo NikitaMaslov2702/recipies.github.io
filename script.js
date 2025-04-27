b6678efdc54c6dac0755f86026b37a54f7d947a8
NikitaMaslov2702 <123866511+NikitaMaslov2702@users.noreply.github.com>
2025-04-27 18:16:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('burg').addEventListener('click', function() {
+    window.location.href = 'page.html';
+});
+
+document.getElementById('logo-title').addEventListener('click', function() {
+    window.location.href = 'main.html'
+})
